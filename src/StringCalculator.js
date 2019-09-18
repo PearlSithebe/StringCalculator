090e8cd8a323dc6b0a7ac53f0ea397856f491fae
PearlSithebe <pearlsithebe@gmail.com>
2019-09-18 12:42:43 +0200

    code modification

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,26 +1,32 @@
-class StringCalculator{
-    constructor(){}
-    Add(numbers) {
-        var sum = 0;
-            if (numbers == "") {
-                return  0;}
-            if (numbers.length == 1){
-                return parseInt(numbers)
-                }
-        let digit = numbers.split(/[/\,;*$!&\n]/);   
-        let neg = digit.filter(function(x){return x < 0})
-        for ( let i = 0; i < digit.length; i++){
-            sum += parseInt(digit[i]);
-            if (digit[i]< 0){
-            
-                throw new Error("negatives not allowed"+ neg)
-                
-        } }
-            return sum
-    }      
+class StringCalculator {
+  Add(numbers) {
+    var sum = 0;
+    if (numbers == "") {
+      return 0;
     }
-            
-    
-        
-    
-        
+    if (numbers.length == 1) {
+      return parseInt(numbers);
+    }
+    let digit = numbers.split(/[,$\n/[;*\]%]+/);
+
+    let neg = digit.filter(function(x) {
+      return x < 0;
+    });
+    for (let i = 0; i < digit.length; i++) {
+      if (digit[i] == "") {
+        digit[i] = 0;
+      }
+
+      if (digit[i] > 1000) {
+        continue;
+      }
+
+      sum += parseInt(digit[i]);
+      if (digit[i] < 0) {
+        throw new Error("negatives not allowed" + neg);
+      }
+    }
+
+    return sum;
+  }
+}
